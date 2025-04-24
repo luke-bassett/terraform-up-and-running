46afcd5efe96b707c306b99477ed5c678efb7587
Luke Bassett <bassett.luke@gmail.com>
2025-04-23 21:03:31 -0700

    add variable server_port and output public_ip

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "hello world" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   user_data_replace_on_change = true
@@ -23,9 +23,22 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port = 8080
-    to_port   = 8080
+    from_port = var.server_port
+    to_port   = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# input variable - keep it DRY
+variable "server_port" {
+  description = "the port the server will use for http requests"
+  type        = number
+  default     = 8080
+}
+
+# output variable - will show after apply
+output "public_ip" {
+  description = "the public IP of the web server"
+  value       = aws_instance.example.public_ip
 }
